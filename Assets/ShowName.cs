6bef905a916d44a7d36dd6c9223727898d7a03f3
BeardyKing <khopking@gmail.com>
2019-06-02 19:48:50 +0100

    capture fix added (hard code)
    todo : build not working

---
+++ Assets/ShowName.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowName : MonoBehaviour
+{
+    public TextMesh tm;
+    int count = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < 0) {
+
+        }
+        if (count < -1) {
+            tm.text = gameObject.name;
+        }
+        if (count > -2) {
+            count--;       
+        }
+    }
+}
